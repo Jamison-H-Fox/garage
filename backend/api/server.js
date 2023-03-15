037d85f4644861dbde82e3fdaebc61761a110acf
Jamison Fox <fox.jamison@outlook.com>
2023-03-15 14:28:19 -0600

    added users and basic flesh out

--- backend/api/server.js
+++ backend/api/server.js
@@ -4,11 +4,13 @@ const server = express();
 server.use(express.json());
 
 const carsRouter = require('./cars/carsRouter');
+const usersRouter = require('./users/usersRouter');
 
 server.use('/api/cars', carsRouter);
+server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
-    res.status(200).json({ message: 'welcome to dream car api' })
+    res.status(200).json({ message: 'welcome to the garage api' })
 })
 
 server.use((err, req, res, next) => { // eslint-disable-line
